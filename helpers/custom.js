71ae6da25e350fc2ed2e143a1a0b113e2337e3d6
Punvarat2534 <72251854+Punvarat2534@users.noreply.github.com>
2025-05-01 00:35:38 +0700

    'change'

--- helpers/custom.js
+++ helpers/custom.js
@@ -3,7 +3,7 @@
 export const useGlobal = () => ({
 	
 	async url_endpoint() {
-		return "http://127.0.0.1:5000";
+		return "http://localhost:5000";
 
 	},
 	hello: function () {
@@ -17,7 +17,7 @@ export const useGlobal = () => ({
 				},
 			};
 		
-		const results = await axios.get("http://127.0.0.1:5000/xcrfstoken",config)
+		const results = await axios.get("http://localhost:5000/xcrfstoken",config)
 		.then(response => {	
 			  this.tokens = response.data.token;   
 			  return response.data.token;        
@@ -29,37 +29,4 @@ export const useGlobal = () => ({
 		return results;
 	}
 });
-
-
-/*export default {
-    data() {
-        return {
-            url_endpoint:"http://127.0.0.1:5000"
-        };
-    },mounted() {
-
-    },
-    computed: {
-    },
-    methods:{
-		async xcrfstokens(){
-
-			const config = {
-					headers: {
-					"x-api-key": "d5168cf6-d8e2-4701-9f8f-d51fe287d73a",
-					},
-				};
-			
-			const results = await axios.get(this.url_endpoint+"/xcrfstoken",config)
-			.then(response => {	
-				  this.tokens = response.data.token;   
-				  return response.data.token;        
-			}).catch(error => {
-				  alert(error);
-			});
-	
-			return results;
-		}
-	}
-};*/
 
